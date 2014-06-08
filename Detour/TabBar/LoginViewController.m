2890ce30780211cba83ce413ef6ab401bc19baf0
Sebastian Ramirez <sramirez10r@gmail.com>
2014-06-08 05:18:35 -0600

    Login Screen

--- Detour/TabBar/LoginViewController.m
+++ Detour/TabBar/LoginViewController.m
@@ -13,13 +13,14 @@
 
 @end
 
-@implementation LoginViewController
+@implementation LoginViewController {
+    UIScrollView *fondo;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
@@ -27,15 +28,50 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    fondo = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+    fondo.userInteractionEnabled = NO;
+    fondo.contentSize = CGSizeMake(393, 568);
+    fondo.backgroundColor = [UIColor blackColor];
+    
+    UIView *bgImage = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 393, 568)];
+    bgImage.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"cabro"]];
+    [fondo addSubview:bgImage];
+    [self.view addSubview:fondo];
+    
+    UIImageView *logo = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"logo"]];
+    logo.center = CGPointMake(320/2, 150);
+    [self.view addSubview:logo];
+    
+    UIImageView *liveLabel = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"live"]];
+    liveLabel.center = CGPointMake(500, 127);
+    [self.view addSubview:liveLabel];
+    
+    UIImageView *signUp = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"signup"]];
+    signUp.center = CGPointMake(81, 700);
+    UIImageView *login = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"login"]];
+    login.center = CGPointMake(237, 700);
+    UITapGestureRecognizer *newTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(login)];
+    [login setUserInteractionEnabled:YES];
+    [login addGestureRecognizer:newTap];
+    [self.view addSubview:signUp];
+    [self.view addSubview:login];
+    
+    [UIView animateWithDuration: 2 delay:2 options:nil animations:^{
+        logo.center = CGPointMake(-200, 150);
+        [fondo setContentOffset:CGPointMake(56, 0) animated:YES];
+        liveLabel.center = CGPointMake(95, liveLabel.center.y);
+        login.center = CGPointMake(login.center.x, 530);
+        signUp.center = CGPointMake(signUp.center.x, 530);
+    } completion:^(BOOL finished) {
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-- (IBAction)login:(id)sender {
+- (void)login {
     AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
     [appDelegate.window setRootViewController:appDelegate.tabBarController];
 }
